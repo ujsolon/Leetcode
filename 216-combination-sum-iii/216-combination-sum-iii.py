911f4fbc9410730c366cbe56f60473406aa49d3e
ujsolon <60507757+ujsolon@users.noreply.github.com>
2022-05-10 23:04:25 +0800

    Time: 48 ms (40.69%), Space: 13.9 MB (79.39%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.py
@@ -0,0 +1,14 @@
+def combs(a):
+    if len(a) == 0:
+        return [[]]
+    cs = []
+    for c in combs(a[1:]):
+        cs += [c, c+[a[0]]]
+    return cs
+
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        digits = [1,2,3,4,5,6,7,8,9]
+        klength = [x for x in combs(digits) if len(x)==k]
+        nsum = [x for x in klength if sum(x)==n]
+        return nsum
