86d2c6a65ba44088a4c41946a2864bd3f0dd419d
ujsolon <60507757+ujsolon@users.noreply.github.com>
2022-05-11 16:50:47 +0800

    Time: 57 ms (29.99%), Space: 13.9 MB (65.31%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.py
@@ -1,6 +1,14 @@
+'''
 import itertools
 
 class Solution:
     def countVowelStrings(self, n: int) -> int:
         vowels = ["a","e","i","o","u"]
         return(len(list(itertools.combinations_with_replacement(vowels, n))))
+'''
+
+import math
+m=5
+class Solution:
+    def countVowelStrings(self, n: int) -> int:
+        return(int(math.factorial(m+n-1)/(math.factorial(n)*math.factorial(m-1))))
