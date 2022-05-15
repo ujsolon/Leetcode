3293abcaa07b6670b31fa00afbdfb1073962c71b
ujsolon <60507757+ujsolon@users.noreply.github.com>
2022-05-16 02:24:00 +0800

    Time: 409 ms (14.58%), Space: 17.9 MB (8.05%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+'''
+https://leetcode.com/problems/deepest-leaves-sum/discuss/2040254/Python-concise-and-readable-BFS-solution
+'''
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        queue = [root]
+        while queue: # iterates through the layers
+            lastSum, newQueue = 0, list() # init sum to zero
+            while queue: # current layer's nodes
+                node = queue.pop(0)
+                lastSum += node.val # sum of the last layer is accumulated here
+                if any([node.left, node.right]): # build next layer
+                    if node.left: newQueue.append(node.left)
+                    if node.right: newQueue.append(node.right)
+            queue = newQueue # next layer
+        return lastSum
