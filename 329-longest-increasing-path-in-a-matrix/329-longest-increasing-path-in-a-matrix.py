b0788f83dd55e9105b94b7d3897886e1bfce385c
ujsolon <60507757+ujsolon@users.noreply.github.com>
2022-05-20 07:59:14 +0800

    Time: 599 ms (53.77%), Space: 19.5 MB (18.06%) - LeetHub

---
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.py
@@ -0,0 +1,31 @@
+'''
+https://leetcode.com/problems/longest-increasing-path-in-a-matrix/discuss/2052360/Python%3A-Beginner-Friendly-%22Recursion-to-DP%22-Intuition-Explained
+'''
+
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        longest_by_pos={}
+        deltas=[(1,0),(-1,0),(0,1),(0,-1)]
+        ROWS,COLS = len(matrix),len(matrix[0])
+        
+        def dfs(pos):
+            if pos in longest_by_pos:
+                return longest_by_pos[pos]
+            
+            val=matrix[pos[0]][pos[1]]
+            longest=1
+            for delta_x,delta_y in deltas:
+                neighbor_x=delta_x+pos[0]
+                neighbor_y=delta_y+pos[1]
+                
+                if 0<=neighbor_x<ROWS and 0<=neighbor_y<COLS and matrix[neighbor_x][neighbor_y]>val:
+                    longest=max(longest,1+dfs((neighbor_x,neighbor_y)))
+            
+            longest_by_pos[pos]=longest
+            return longest
+        
+        max_path=1
+        for row in range(ROWS):
+            for col in range(COLS):
+                max_path=max(max_path,dfs((row,col)))
+        return max_path
