6bdbb49360bb6990ffecfd11e1e6dd6a3a609c94
ujsolon <60507757+ujsolon@users.noreply.github.com>
2022-06-09 01:56:00 +0800

    Time: 26 ms (94.75%), Space: 13.8 MB (53.87%) - LeetHub

---
+++ 1332-remove-palindromic-subsequences/1332-remove-palindromic-subsequences.py
@@ -0,0 +1,39 @@
+'''
+import itertools as it
+
+def listtostr(l:list)-> str:
+    ans=""
+    for x in l:
+        ans+=x
+    return ans
+
+def isPalindrome(s:str)->bool:
+    return(s==s[::-1])
+
+class Solution:
+    def removePalindromeSub(self, s: str) -> int:
+        base='ab'
+        lnnn=[]
+        count=0
+        for k in range(1,len(s)+1):
+            l = list(it.product(base,repeat=k))
+            ln = [listtostr(item) for item in l]
+            lnn = [item for item in ln if isPalindrome(item)==True]
+            lnnn.append(lnn)
+        lnnn.reverse() ##reverse the order of palindromic substrings from the longest to the shortest
+
+        for i in range(len(s)):
+            #print(i)
+            for string in lnnn[i]:
+                #print(string)
+                while string in s:
+                    #print(string)
+                    count+=1
+                    s=s.replace(string,'')
+        return(count)
+'''
+
+##This one liner of a solution takes into account an observation that the max number of steps here is 2
+class Solution:
+    def removePalindromeSub(self, s: str) -> int:
+        return 1 if s == s[::-1] else 2
